2ca6082bdfc896ac6f6834e5afa710f590e29f93
Sakai-sam <o_edo@hotmail.com>
2021-05-31 16:52:58 +0200

    Cleanup.

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,24 @@
 import React from 'react';
 import Loader from './Loader';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
-      <div style={{ display: 'flex' }}>
+    <div>
+      <div className="Grid">
         <Loader>
           {
-            (onLoad) => <img src='https://miro.medium.com/max/12000/1*mbZ4d3IzTmi46SzfihlJqw.jpeg' onLoad={onLoad} width={400} height={300} />
+            (onLoad) => <img src='https://miro.medium.com/max/12000/1*mbZ4d3IzTmi46SzfihlJqw.jpeg' onLoad={onLoad} />
           }
         </Loader>
         <Loader>
           {
-            (onLoad) => <img src='https://miro.medium.com/max/12000/1*mbZ4d3IzTmi46SzfihlJqw.jpeg' onLoad={onLoad} width={400} height={300} />
+            (onLoad) => <img src='https://miro.medium.com/max/12000/1*mbZ4d3IzTmi46SzfihlJqw.jpeg' onLoad={onLoad} />
           }
         </Loader>
         <Loader>
           {
-            (onLoad) => <img src='https://miro.medium.com/max/12000/1*mbZ4d3IzTmi46SzfihlJqw.jpeg' onLoad={onLoad} width={400} height={300} />
+            (onLoad) => <img src='https://miro.medium.com/max/12000/1*mbZ4d3IzTmi46SzfihlJqw.jpeg' onLoad={onLoad} />
           }
         </Loader>
         <Loader>
