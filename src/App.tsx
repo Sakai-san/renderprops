e6ae13d0a74663422a0c963933ea2f0f8973ab57
Sakai-sam <o_edo@hotmail.com>
2021-06-01 07:32:51 +0200

    Logging.

--- src/App.tsx
+++ src/App.tsx
@@ -8,22 +8,36 @@ function App() {
       <div className="Grid">
         <Loader>
           {
-            (onLoad) => <img src='https://miro.medium.com/max/12000/1*mbZ4d3IzTmi46SzfihlJqw.jpeg' onLoad={onLoad} />
+            (onLoad) => {
+              console.log('image erable');
+              return <img src='https://miro.medium.com/max/12000/1*mbZ4d3IzTmi46SzfihlJqw.jpeg' onLoad={onLoad} width={400} height={300} />;
+
+            }
           }
         </Loader>
         <Loader>
           {
-            (onLoad) => <img src='https://miro.medium.com/max/12000/1*mbZ4d3IzTmi46SzfihlJqw.jpeg' onLoad={onLoad} />
+            (onLoad) => {
+              console.log('image jungfrau');
+              return <img src='https://media.tacdn.com/media/attractions-splice-spp-674x446/0b/2f/ff/85.jpg' onLoad={onLoad} width={400} height={300} />;
+            }
+            
           }
         </Loader>
         <Loader>
           {
-            (onLoad) => <img src='https://miro.medium.com/max/12000/1*mbZ4d3IzTmi46SzfihlJqw.jpeg' onLoad={onLoad} />
+            (onLoad) => {
+              console.log('image walensee');
+              return <img src='https://swissfamilyfun.com/wp-content/uploads/2021/03/wildflowers-brienz-rothorn-ridge.jpg' onLoad={onLoad} width={400} height={300} />;
+            }
           }
         </Loader>
         <Loader>
           {
-            (onLoad) => <iframe src='https://wikipedia.org' onLoad={onLoad} width={400} height={300} />
+            (onLoad) => {
+              console.log('iframe wikipedia');
+              return <iframe src='https://wikipedia.org' onLoad={onLoad} width={400} height={300} />;
+            }
           }
         </Loader>
       </div>
